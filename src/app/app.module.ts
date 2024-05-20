c4d5a523dd13d559874ae1503e870f5dd81b77bf
andreirosu97 <andreirosu97@gmail.com>
2024-05-20 16:13:04 +0300

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListViewComponent } from './components/list-view/list-view.component';
-import { LocationSelectorComponent } from './components/location-selector/location-selector.component';
+import {ListViewComponent} from "./components/list-view/list-view.component";
+import {LocationSelectorComponent} from "./components/location-selector/location-selector.component";
 
 @NgModule({
-  declarations: [AppComponent, ListViewComponent, LocationSelectorComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ListViewComponent,
+    LocationSelectorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
